8e93601141a594ce07a3ec0652fd930b2bd4fe4f
kaush <emailtokaush@gmail.com>
2020-08-06 19:24:37 -0400

    remove unwanted comments in dtos

--- project-management/src/main/java/com/kaush/pma/dto/ChartData.java
+++ project-management/src/main/java/com/kaush/pma/dto/ChartData.java
@@ -4,9 +4,5 @@ public interface ChartData {
 
 	public String getLabel(); 
 	public long getValue();
-	
-	// these should exactly same as db table we fetch using query or assign table value in query
-	//SELECT stage as label, COUNT(*) as value FROM project GROUP BY stage")
-	 //label
-	//value
+
 }
